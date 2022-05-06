db879b4ea98dd0904e8696270aa089e6de140f3f
Garett Brown <gbrown@physics.utoronto.ca>
2022-05-06 17:12:55 -0400

    Added more details. Moved functions to seperate file.

--- flybys.py
+++ flybys.py
@@ -2,29 +2,93 @@ import rebound
 import numpy as np
 import matplotlib.pyplot as plt
 
+################################
+###### Useful Constants ########
+################################
+
 twopi = 2.*np.pi
 kms_to_auyr2pi = 0.03357365989646266 # 1 km/s to AU/Yr2Pi
 pc3_to_au3 = ((np.pi**3)/272097792000000000) # 1 parsec^-3 to AU^-3
 
-def vinf_and_b_to_e(Mc, m, Ms, b, vinf):
+
+
+
+############################################################
+### Stellar Mass generators using Initial Mass Functions ###
+############################################################
+
+def imf_gen_1(size):
+    '''
+        Generate stellar mass samples for single star systems between 0.01 and 1.0 Solar Mass.
+        
+        Computed using the inverted cummulative probability distribution (CDF) from the initial mass function (IMF) given in equation (17) by Chabrier (2003) https://ui.adsabs.harvard.edu/abs/2003PASP..115..763C/abstract
+
+        Parameters
+        ----------
+        size: the number of samples to draw.
+    '''
+    n = int(size)
+    u = uniform.rvs(size=n)
+    return 0.08*np.exp(2.246879476250902 * erfinv(-0.8094067254228074 + 1.6975098420629455*u))
+
+def imf_gen_10(size):
+    '''
+        Generate stellar mass samples for single star systems between 0.01 and 10.0 Solar Masses.
+        
+        Computed using the inverted cummulative probability distribution (CDF) by smoothly combining the initial mass function (IMF) given in equation (17) by Chabrier (2003) https://ui.adsabs.harvard.edu/abs/2003PASP..115..763C/abstract for stars less than 1.0 Solar Mass with the standard power-law IMF from Salpeter (1955) https://ui.adsabs.harvard.edu/abs/1955ApJ...121..161S/abstract for stars more than 1.0 Solar Mass.
+
+        Parameters
+        ----------
+        size: the number of samples to draw.
+    '''
+    n = int(size)
+    u = uniform.rvs(size=n)
+    return np.where(u > 0.9424222533172513, 0.11575164791201686 / (1.0030379829867349 - u)**(10/13.), 0.08*np.exp(2.246879476250902 * erfinv(-0.8094067254228074 + 1.801220032833315*u)))
+
+def imf_gen_100(size):
+    '''
+        Generate stellar mass samples for single star systems between 0.01 and 100.0 Solar Masses.
+        
+        Computed using the inverted cummulative probability distribution (CDF) by smoothly combining the initial mass function (IMF) given in equation (17) by Chabrier (2003) https://ui.adsabs.harvard.edu/abs/2003PASP..115..763C/abstract for stars less than 1.0 Solar Mass with the standard power-law IMF from Salpeter (1955) https://ui.adsabs.harvard.edu/abs/1955ApJ...121..161S/abstract for stars more than 1.0 Solar Mass.
+
+        Parameters
+        ----------
+        size: the number of samples to draw.
+    '''
+    n = int(size)
+    u = uniform.rvs(size=n)
+    return np.where(u > 0.9397105089399359, 0.11549535807627886 / (1.0001518217134586 - u)**(10/13.), 0.08*np.exp(2.246879476250902 * erfinv(-0.8094067254228074 + 1.8064178551944312*u)))
+
+
+
+
+
+############################################################
+################### Helper Functions #######################
+############################################################
+
+def vinf_and_b_to_e(mu, b, vinf):
     '''
         Using the impact parameter to convert from the relative velocity at infinity between the two stars to the eccentricity of the flyby star.
         Equation (2) from Spurzem et al. (2009) https://ui.adsabs.harvard.edu/abs/2009ApJ...697..458S/abstract
 
         Parameters
         ----------
-        Mc : mass of the central star (ex. the Sun)
-        m : mass of the planet orbiting the central star
-        Ms : mass of the flyby star
-        b :  impact parameter of the flyby star
-        vinf : the relative velocity at infinity between the central star and the flyby star (hyperbolic excess velocity)
+        mu : the total mass of the system (Sun, planets, and flyby star) times the gravitational constant G in units where G = 1, Msun, AU, Yr2Pi
+        b :  impact parameter of the flyby star in units of AU
+        vinf : the relative velocity at infinity between the central star and the flyby star (hyperbolic excess velocity) in units of km/s
     '''
-    G = 1 # Use units where G = 1, Msun, AU, Yr2Pi
     v = vinf*kms_to_auyr2pi # Convert velocity units from km/s to AU/Yr2Pi
-    M123 = Mc + m + Ms # Compute sum of the masses
     numerator = b * v**2.
-    denominator = G * M123
-    return np.sqrt(1 + (numerator/denominator)**2.)
+    return np.sqrt(1 + (numerator/mu)**2.)
+
+
+
+
+############################################################
+#################### Flyby Functions #######################
+############################################################
+
 
 def flyby(sim, star_mass=1, star_b=100,  star_e=None, star_v=None, star_omega='uniform', star_Omega='uniform', star_inc='uniform', showflybysetup=False):
     '''
@@ -34,12 +98,14 @@ def flyby(sim, star_mass=1, star_b=100,  star_e=None, star_v=None, star_omega='u
         Any changes made inside this function to the REBOUND simulation are permanent.
         This function assumes that you are using a WHFAST integrator with REBOUND.
     '''
+    
+    mu = sim.G * (np.sum([p.m for p in sim.particles]) + star_mass)
     if star_e is None and star_v is not None:
         # If `star_v` is defined convert it to eccentricity.
         # Assumes that `star_v` is in units of km/s.
         sun_mass = sim.particles[0].m
         planet_mass = sim.particles[1].m
-        e = vinf_and_b_to_e(Mc=sun_mass, m=planet_mass, Ms=star_mass, b=star_b, vinf=star_v)
+        e = vinf_and_b_to_e(mu=mu, b=star_b, vinf=star_v)
     elif star_e is not None and star_v is None:
         # Simply use the eccentricity if it is defined.
         e = star_e
@@ -59,7 +125,6 @@ def flyby(sim, star_mass=1, star_b=100,  star_e=None, star_v=None, star_omega='u
     # Calculate half of the integration time for the flyby star.
     E = np.arccosh((np.cos(f)+e)/(1.+e*np.cos(f))) # Compute the eccentric anomaly
     M = e * np.sinh(E)-E # Compute the mean anomaly
-    mu = sim.G * (np.sum([p.m for p in sim.particles]) + star_mass)
     tperi = M/np.sqrt(mu/(-a*a*a)) # Compute the time to pericentre (-a because the semi-major axis is negative)
 
     #################################################
