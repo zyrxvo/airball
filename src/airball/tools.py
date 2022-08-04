5a7958d6c4bfc942ed1f6da9eb9a6afb9a4f0d46
Garett Brown <gbrown@physics.utoronto.ca>
2022-08-04 16:11:20 -0400

    Added a couple constants.

--- src/airball/tools.py
+++ src/airball/tools.py
@@ -1,4 +1,8 @@
+from lib2to3.pytree import convert
 import numpy as _numpy
+
+convert_kms_to_auyr2pi = 0.03357365989646266 # 1 km/s to AU/Yr2Pi
+convert_auyr2pi_to_kms = 1/convert_kms_to_auyr2pi
 
 def moving_average(a, n=3) :
     ret = _numpy.cumsum(a)
