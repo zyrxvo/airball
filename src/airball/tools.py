9981873b2efd426cf96e18e2e2b61b86672d92de
Garett Brown <gbrown@physics.utoronto.ca>
2022-10-20 13:13:00 -0400

    Added credit for moving average alorithm.

--- src/airball/tools.py
+++ src/airball/tools.py
@@ -4,7 +4,9 @@ import numpy as _numpy
 convert_kms_to_auyr2pi = 0.03357365989646266 # 1 km/s to AU/Yr2Pi
 convert_auyr2pi_to_kms = 1/convert_kms_to_auyr2pi
 
+# Implemented from StackOverflow: https://stackoverflow.com/a/14314054
 def moving_average(a, n=3) :
+    '''Compute the moving average of an array of numbers using the nearest n elements.'''
     ret = _numpy.cumsum(a)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
