fea4b418c85e0d93379a355e38365bea547197ca
Garett Brown <garett_brown@icloud.com>
2023-08-14 13:38:26 +0900

    Added alias for Solar Mass

--- src/airball/units.py
+++ src/airball/units.py
@@ -1,4 +1,7 @@
 from astropy.units import *
 twopi = 6.28318530717958623199592693708837032318115234375
-yr2pi = def_unit('yrtwopi', yr/twopi, format={'latex': r'(yr/2\pi)'})
+yrtwopi = def_unit('yrtwopi', yr/twopi, format={'latex': r'(yr/2\pi)'})
+yr2pi = def_unit('yr2pi', yr/twopi, format={'latex': r'(yr/2\pi)'})
 stars = def_unit('stars')
+add_enabled_units([yr2pi, yrtwopi])
+add_enabled_aliases({'msun': solMass})
