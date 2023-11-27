b75a59d247a63fdbaa1190f282fb858194abbbb1
Garett Brown <garett_brown@icloud.com>
2023-11-27 10:53:08 +0900

    v0.5.0

--- src/airball/__init__.py
+++ src/airball/__init__.py
@@ -2,7 +2,7 @@
 # Flybys in `REBOUND`
 - A package for implementing flybys in [hannorein/rebound](https://github.com/hannorein/rebound)
 '''
-from .core import (add_star_to_sim, flyby, flybys, hybrid_flyby, hybrid_flybys)
+from .core import (add_star_to_sim, flyby, flybys, successive_flybys, concurrent_flybys)
 from .environments import (StellarEnvironment, LocalNeighborhood, OpenCluster, GlobularCluster, GalacticBulge, GalacticCore)
 from .analytic import (relative_energy_change, energy_change_adiabatic_estimate, eccentricity_change_adiabatic_estimate)
 from .imf import (IMF)
