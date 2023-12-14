bd5d8f1cc620b6bcdb12d060d1a930f821afae1c
Garett Brown <garett_brown@icloud.com>
2023-12-14 15:28:46 +0900

    Added basic test of isList.

---
+++ tests/tools.py
@@ -0,0 +1,18 @@
+import pytest
+import airball
+import airball.units as u
+import numpy as np
+
+def test_isList():
+    arr1 = [1,2,3]
+    arr2 = np.array([1, 2, 3])
+    arr3 = [1,2,3] << u.km
+    val1 = 1
+    val2 = np.array([1])
+    val3 = 1 << u.km
+    assert airball.tools.isList(arr1)
+    assert airball.tools.isList(arr2)
+    assert airball.tools.isList(arr3)
+    assert not airball.tools.isList(val1)
+    assert airball.tools.isList(val2) # an ndarray is always a list
+    assert not airball.tools.isList(val3) # a single Quanity is not a list
