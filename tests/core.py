8ce88530ae177fb09f3af010bdd8547b1eda23b7
Garett Brown <garett_brown@icloud.com>
2023-12-14 14:44:31 +0900

    Updated basic core tests.

--- tests/core.py
+++ tests/core.py
@@ -11,19 +11,32 @@ import numpy as np
 ################################################
 
 def test_add_star():
-    star = airball.Star(m=0, b=1, v=1, inc=0, omega=0, Omega=0)
+    star = airball.Star(m=1, b=1, v=1, inc=1, omega=1, Omega=1)
     sim = rebound.Simulation()
     sim.add(m=1)
-    airball.add_star_to_sim(sim, star, rmax=1e5, hash='star')
+    RMAX = 1e5*u.au
+    el1 = airball.tools.hyperbolic_elements(sim, star, rmax=RMAX, values_only=True)
+    airball.add_star_to_sim(sim, star, rmax=RMAX, hash='star')
+    el0 = sim.orbits(primary=sim.particles[0])[0]
     assert sim.N == 2
+    assert np.isclose(el0.a, el1['a'])
+    assert np.isclose(el0.e, el1['e'])
+    assert np.isclose(el0.inc, el1['inc'])
+    assert np.isclose(el0.omega, el1['omega'])
+    assert np.isclose(el0.Omega, el1['Omega'])
 
 def test_add_star_to_empty_sim():
-    star = airball.Star(m=0, b=1, v=1, inc=0, omega=0, Omega=0)
+    star = airball.Star(m=1, b=1, v=1, inc=0, omega=0, Omega=0)
     sim = rebound.Simulation()
     with pytest.raises(AttributeError):
         airball.add_star_to_sim(sim, star, rmax=1e5, hash='star')
 
 def test_remove_star():
+    sim = rebound.Simulation()
+    sim.add(m=0, hash='flybystar')
+    airball.core.remove_star_from_sim(sim, 'flybystar')
+    assert sim.N == 0
+
     sim = rebound.Simulation()
     with pytest.raises(RuntimeError):
         airball.core.remove_star_from_sim(sim, 'star')
@@ -36,8 +49,10 @@ def test_remove_star():
 ################################################
 
 def test_basic_flyby():
-    star = airball.Star(m=0, b=1, v=1, inc=0, omega=0, Omega=0)
+    star = airball.Star(m=1, b=1, v=1, inc=0, omega=0, Omega=0)
     sim = rebound.Simulation()
     sim.add(m=1)
     simout = airball.flyby(sim, star)
-    assert sim.N == simout.N
+    assert sim == simout
+    simout = airball.flyby(sim, star, overwrite=False)
+    assert sim != simout
