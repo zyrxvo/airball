78b20c57ec6ae200ec49b6479d9777944df00a28
Garett Brown <garett_brown@icloud.com>
2024-06-27 09:59:38 -0400

    Fixed random star generation from uniform to isotropic.

--- src/airball/environments.py
+++ src/airball/environments.py
@@ -84,7 +84,7 @@ class StellarEnvironment:
 
   def random_stars(self, size=1, **kwargs):
     '''
-      Computes a random star from a stellar environment.
+      Computes a isotropically random star from a stellar environment.
 
       Args:
         size (int or tuple): The number of stars to generate. If size is a tuple, it is interpreted as array dimensions. Default: 1.
@@ -122,9 +122,9 @@ class StellarEnvironment:
     m = self.IMF.random_mass(size=size) # Mass of the star.
 
     zeros = _np.zeros(size)
-    inc = (2.0*_np.pi * _uniform.rvs(size=size) - _np.pi) << self.units['angle'] if include_orientation else zeros
-    ω = (2.0*_np.pi * _uniform.rvs(size=size) - _np.pi) << self.units['angle'] if include_orientation else zeros
-    Ω = (2.0*_np.pi * _uniform.rvs(size=size) - _np.pi) << self.units['angle'] if include_orientation else zeros
+    inc = (2*_np.arcsin(_np.sqrt(_uniform.rvs(size=size)))) << self.units['angle'] if include_orientation else zeros
+    ω = (_uniform.rvs(loc=0, scale=(2.0*_np.pi), size=size)) << self.units['angle'] if include_orientation else zeros
+    Ω = (_uniform.rvs(loc=-_np.pi, scale=(2.0*_np.pi), size=size)) << self.units['angle'] if include_orientation else zeros
 
     if isinstance(size, tuple): return _Stars(m=m, b=b, v=v, inc=inc, omega=ω, Omega=Ω, UNIT_SYSTEM=self.UNIT_SYSTEM, environment=self)
     elif size > 1: return _Stars(m=m, b=b, v=v, inc=inc, omega=ω, Omega=Ω, UNIT_SYSTEM=self.UNIT_SYSTEM, environment=self)
